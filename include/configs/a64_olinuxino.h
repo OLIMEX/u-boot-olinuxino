f59b1f0024d97c41c27cd031e10aacc1a77d4ca7
Stefan Saraev <stefan@saraev.ca>
2020-05-20 16:25:49 +0300

    add: initial olinuxino support

---
+++ include/configs/a64_olinuxino.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020 Olimex Ltd.
+ *   Author: Stefan Mavrodiev <stefan@olimex.com>
+ *
+ * SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+ */
+#ifndef __A64_OLINUXINO_H
+#include <configs/sun50i.h>
+
+#define LCD_OLINUXINO_EEPROM_BUS	0
+#define LCD_OLINUXINO_EEPROM_ADDRESS	0x50
+#endif /* __A64_OLINUXINO_H */
